0ee7ca792922dfa829e05e773f93ef074d7e731a
DevRobot <ysrosast@gmail.com>
2024-10-10 20:51:19 +0200

    White noise loading

--- app/components/game/spinner.tsx
+++ app/components/game/spinner.tsx
@@ -1,9 +1,9 @@
-import { RefreshCcwIcon } from "lucide-react";
+import Image from "next/image";
 
 export default function Spinner() {
   return (
-    <section className="flex items-center justify-center w-[1280px] min-h-[853px] gap-8 bg-gray-200 rounded" >
-      <RefreshCcwIcon className="text-red-950 h-12 w-12 animate-spin" />
+    <section className="flex items-center justify-center w-[912px] min-h-[610px] gap-8 bg-black rounded" >
+      <Image className="object-fill w-[912px] h-[600px]" src={"/white-noise.gif"} alt="spinner" width={912} height={600} />
     </section>
   );
 }
