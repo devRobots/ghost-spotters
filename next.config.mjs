f2ec718525877a5211328a7bf686ace8bf0f8600
DevRobot <ysrosast@gmail.com>
2024-10-10 13:00:30 +0200

    Clean up

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'pixabay.com',
-                pathname: '/get/**',
-            },
-        ],
-    },
     reactStrictMode: false,
 };
 
