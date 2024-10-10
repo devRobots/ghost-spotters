99acb231cd20fedd3516712e09e7aa8c375ddfc7
DevRobot <ysrosast@gmail.com>
2024-10-10 14:26:22 +0200

    Ghosts absolute position fix

--- app/components/game/ghost.tsx
+++ app/components/game/ghost.tsx
@@ -25,11 +25,8 @@ export default function GhostImage(
   }, [inGame])
 
   return (
-    <div className={
-      `absolute top-[${y}px] left-[${x}px] w-[${w}px] min-h-[${h}px] ` +
-      (spotted ? "border-red-600 border-2" : "")
-    }>
-      <Image
+    <div style={{ position: "absolute", top: y, left: x, width: w, height: h }} className={spotted ? "border-red-600 border-2" : ""}>
+      <Image style={{ opacity: 0.8 }}
         alt="ghost"
         onClick={handleClick}
         src={`/ghosts/ghost-${index}.png`}
