23e583ca217b49ae59dec8bca6530c4ad6c7b60d
DevRobot <ysrosast@gmail.com>
2024-10-10 02:51:53 +0200

    Home Game

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,33 @@
-import { Button } from "@/components/ui/button"
+import { Suspense } from "react";
 
-export default function Home() {
+import Score from "@/components/game/score";
+import Combo from "@/components/game/combo";
+import Timer from "@/components/game/timer";
+import FindScore from "@/components/game/finds";
+import ImageSpotter from "@/components/game/image";
+
+import { GameContextProvider } from "@/context/GameContext";
+import Spinner from "@/components/game/spinner";
+
+export default function Game() {
   return (
-    <main className="grid place-content-center min-h-screen w-screen">
-      <h1 className="text-center">
-        Spot-IA
-      </h1>
-      <Button size="lg" >
-        Jugar
-      </Button>
-    </main>
+    <GameContextProvider>
+      <main className="grid place-content-center min-h-screen w-screen gap-8">
+        <section className="flex flex-row justify-between">
+          <h1 className="text-yellow-700 text-center text-4xl font-bold">
+            GhostSpotters
+          </h1>
+          <section className="flex flex-row gap-4">
+            <Combo />
+            <FindScore />
+            <Score />
+            <Timer />
+          </section>
+        </section>
+        <Suspense fallback={<Spinner />}>
+          <ImageSpotter />
+        </Suspense>
+      </main>
+    </GameContextProvider>
   );
 }
