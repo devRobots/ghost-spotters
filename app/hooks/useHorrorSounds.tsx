26603ef1cdca8a9a11ae57f59a6392661eed3493
DevRobot <ysrosast@gmail.com>
2024-10-20 23:52:40 +0200

    horror sounds improvements

--- app/hooks/useHorrorSounds.tsx
+++ app/hooks/useHorrorSounds.tsx
@@ -1,6 +1,7 @@
-import { useGameStore } from "@/providers/game";
-import { useEffect } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
 import useSound from "use-sound";
+import { useCallback, useEffect } from "react";
+import { useGameStore } from "@/providers/game";
 
 export default function useHorrorSounds() {
   const { isScreaming, stopScream } = useGameStore((state) => state);
@@ -12,32 +13,39 @@ export default function useHorrorSounds() {
   const [playWhisper2, infoWhisper2] = useSound("/sounds/laugh.mp3", { volume: 1, interrupt: true });
   const [playWhisper3, infoWhisper3] = useSound("/sounds/breath.mp3", { volume: 1, interrupt: true });
 
+  const scream = useCallback(() => {
+    console.log("Scream!");
+    const option = Math.random();
+    const info = option <= 0.5 ? infoScream1 : infoScream2;
+
+    if (option <= 0.5) playScream1();
+    else playScream2();
+
+    setTimeout(() => {
+      stopScream();
+    }, info.duration!);
+  }, [stopScream, playScream1, playScream2]);
+
+  const whisper = useCallback(() => {
+    console.log("Whisper!");
+    const option = Math.random();
+    const info = option <= 0.33 ? infoWhisper1 : option <= 0.66 ? infoWhisper2 : infoWhisper3;
+
+    if (option <= 0.33) playWhisper1();
+    else if (option <= 0.66) playWhisper2();
+    else playWhisper3();
+
+    setTimeout(() => {
+      stopScream();
+    }, info.duration!);
+  }, [stopScream, playWhisper1, playWhisper2, playWhisper3]);
 
   useEffect(() => {
     if (isScreaming) {
-      let info = null;
-      const action = Math.random();
-      const option = Math.random();
-
-      if (action >= 0.75) {
-        if (option <= 0.5) playScream1();
-        else playScream2();
-
-        info = option <= 0.5 ? infoScream1 : infoScream2;
-      }
-      else {
-        if (option <= 0.33) playWhisper1();
-        else if (option <= 0.66) playWhisper2();
-        else playWhisper3();
-
-        info = option <= 0.33 ? infoWhisper1 : option <= 0.66 ? infoWhisper2 : infoWhisper3;
-      }
-
-      setTimeout(() => {
-        stopScream();
-      }, info.duration!);
+      if (Math.random() <= 0.5) scream();
+      else whisper();
     }
-  }, [isScreaming, stopScream]);
+  }, [isScreaming, scream, whisper]);
 
   return {};
 }
