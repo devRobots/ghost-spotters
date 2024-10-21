3bc9317f6822075768fa3b4db9e050bfba98f805
DevRobot <ysrosast@gmail.com>
2024-10-21 02:46:12 +0200

    Cache control

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    headers: () => [
+        {
+            source: '/api/image',
+            headers: [
+                { key: 'Cache-Control', value: 'no-store' },
+            ]
+        }
+    ]
 };
 
 export default nextConfig;
